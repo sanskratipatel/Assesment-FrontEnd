a30bbf5920658fb34a017727a9f817b1c15714cf
Sanskrati Patel <patelsanskrati@gmail.com>
2026-06-03 09:40:31 +0530

    Fix issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 ﻿import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/' || 'https://inventory-management-jo7m.onrender.com/',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/',
   headers: {
     'Content-Type': 'application/json'
   },
